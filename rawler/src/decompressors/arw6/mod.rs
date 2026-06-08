a47207d987514ed1bad41761b499dc7c46896802
Rafał Mikrut <mikrutrafal@protonmail.com>
2026-06-08 12:35:00 +0000

    Replace unimplemented!() with error return in decompress_arw6() in arw6/mod.rs

--- rawler/src/decompressors/arw6/mod.rs
+++ rawler/src/decompressors/arw6/mod.rs
@@ -5,5 +5,5 @@ use crate::bits::LookupTable;
 use crate::pixarray::PixU16;
 
 pub(crate) fn decompress_arw6(_buf: &[u8], _width: usize, _height: usize, _curve: &LookupTable, _dummy: bool) -> Result<PixU16> {
-  unimplemented!()
+  Err(crate::RawlerError::DecoderFailed("ARW6 (Sony wavelet) decompressor is not yet implemented".to_string()))
 }
