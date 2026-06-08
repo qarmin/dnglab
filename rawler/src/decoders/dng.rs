1b6461578cd20fa71cebec89d828b1abb20cc142
Rafał Mikrut <mikrutrafal@protonmail.com>
2026-06-08 16:49:34 +0000

    Replace todo!() in DngDecoder photometric match with a proper error for unsupported values

--- rawler/src/decoders/dng.rs
+++ rawler/src/decoders/dng.rs
@@ -63,7 +63,7 @@ impl<'a> Decoder for DngDecoder<'a> {
       1 => RawPhotometricInterpretation::BlackIsZero,
       32803 => RawPhotometricInterpretation::Cfa(CFAConfig::new_from_camera(&cam)),
       34892 => RawPhotometricInterpretation::LinearRaw,
-      _ => todo!(),
+      other => return Err(RawlerError::DecoderFailed(format!("DNG: unsupported photometric interpretation {}", other))),
     };
 
     let raw_data = plain_image_from_ifd(raw, file)?;
